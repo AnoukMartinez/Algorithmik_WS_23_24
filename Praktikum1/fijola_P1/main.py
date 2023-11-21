ec3e38a4f8fb2aa4ffb80f794f1801db31954c23
FijolaC <fijola.czyborski@tutanota.com>
2023-11-21 14:28:35 +0100

    P1-07
    if else blöcke für get hinzugefügt
    für den AVL und den AVL Suggest
    
    Aktuell nur Volltextsuche (ganzes Wort)

--- Praktikum1/fijola_P1/main.py
+++ Praktikum1/fijola_P1/main.py
@@ -68,36 +68,75 @@ def direct_acc_map(filename: str, csv_no: str):
     # print_all(my_direct_acc_map.get_all())
 
 
-def avl(filename: str):
+def avl(filename: str, csv_no: str):
     my_avl = AcNg.AutocompleteNgrams(filename)
-    # x.avl_tree.print_tree()
-    print("my_avl: height: ", my_avl.avl_tree.height())
 
-    # csv 1
-    # print("my_avl get", my_avl.get("eighty-four"))
-    # csv 2
-    # print("my_avl get: ", my_avl.get("and hungry"))
-    # csv 3
-    # print("my_avl get: ", my_avl.get("it from attacking"))
-    # csv 4
-    # print("my_avl get: ", my_avl.get("i quickly discovered that"))
+    if csv_no == 'csv01':
+        # CSV1
+        print("BLOCK CSV01")
+        print("my_avl: height: ", my_avl.avl_tree.height())
+        print("my_avl get: ", my_avl.get("eighty-four"))
+    elif csv_no == 'csv02':
+        # CSV2
+        print("BLOCK CSV02")
+        print("my_avl: height: ", my_avl.avl_tree.height())
+        print("my_avl get: ", my_avl.get("and hungry"))
+    elif csv_no == 'csv03':
+        # CSV3
+        print("BLOCK CSV03")
+        print("my_avl: height: ", my_avl.avl_tree.height())
+        print("my_avl get: ", my_avl.get("it from attacking"))
+    elif csv_no == 'csv04':
+        # CSV4
+        print("BLOCK CSV04")
+        print("my_avl: height: ", my_avl.avl_tree.height())
+        print("my_avl get: ", my_avl.get("i quickly discovered that"))
+
+    # my_avl.avl_tree.print_tree()
 
 
-def avl_suggest(filename: str):
-    my_avl = AcNg.AutocompleteNgrams(filename)
-    print("my_avl: height: ", my_avl.avl_tree.height())
+def avl_suggest(filename: str, csv_no: str):
+    my_avl_suggest = AcNg.AutocompleteNgrams(filename)
+
+    if csv_no == 'csv01':
+        # CSV1
+        print("BLOCK CSV01")
+        print("my_avl_suggest: height: ", my_avl_suggest.avl_tree.height())
+        # print("my_avl_suggest get: ", my_avl_suggest.get("eighty-four"))
+    elif csv_no == 'csv02':
+        # CSV2
+        print("BLOCK CSV02")
+        print("my_avl_suggest: height: ", my_avl_suggest.avl_tree.height())
+        # print("my_avl_suggest get: ", my_avl_suggest.get("and hungry"))
+    elif csv_no == 'csv03':
+        # CSV3
+        print("BLOCK CSV03")
+        print("my_avl_suggest: height: ", my_avl_suggest.avl_tree.height())
+        # print("my_avl_suggest get: ", my_avl_suggest.get("it from attacking"))
+    elif csv_no == 'csv04':
+        # CSV4
+        print("BLOCK CSV04")
+        print("my_avl_suggest: height: ", my_avl_suggest.avl_tree.height())
+        # print("my_avl_suggest get: ", my_avl_suggest.get("i quickly discovered that"))
 
 
 if __name__ == '__main__':
-    linked_dict("csv/1_gram.csv", "csv01")
-    linked_dict("csv/2_gram.csv", "csv02")
-    linked_dict("csv/3_gram.csv", "csv03")
-    linked_dict("csv/4_gram.csv", "csv04")
+    # linked_dict("csv/1_gram.csv", "csv01")
+    # linked_dict("csv/2_gram.csv", "csv02")
+    # linked_dict("csv/3_gram.csv", "csv03")
+    # linked_dict("csv/4_gram.csv", "csv04")
 
-    direct_acc_map("csv/1_gram.csv", "csv01")
-    direct_acc_map("csv/2_gram.csv", "csv02")
-    direct_acc_map("csv/3_gram.csv", "csv03")
-    direct_acc_map("csv/4_gram.csv", "csv04")
+    # direct_acc_map("csv/1_gram.csv", "csv01")
+    # direct_acc_map("csv/2_gram.csv", "csv02")
+    # direct_acc_map("csv/3_gram.csv", "csv03")
+    # direct_acc_map("csv/4_gram.csv", "csv04")
 
-    #  avl("csv/1_gram.csv")
-    # avl_suggest("csv/1_gram.csv")
+    # avl("csv/1_gram.csv", "csv01")
+    # avl("csv/2_gram.csv", "csv02")
+    # avl("csv/3_gram.csv", "csv03")
+    # avl("csv/4_gram.csv", "csv04")
+
+    avl_suggest("csv/1_gram.csv", "csv01")
+    avl_suggest("csv/2_gram.csv", "csv02")
+    avl_suggest("csv/3_gram.csv", "csv03")
+    avl_suggest("csv/4_gram.csv", "csv04")
